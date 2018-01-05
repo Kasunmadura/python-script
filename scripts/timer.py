c128478e60c0a35e26109cb7ad9406a6a1143f41
kasunmaduraeng <kasunmaduraeng@gmail.com>
2018-01-05 14:52:05 +1300

    remove total function requiment

--- scripts/timer.py
+++ scripts/timer.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python
 
-import time
+#import time
+from time import localtime, strftime , mktime
 
-start_time = time.localtime()
+#start_time = time.localtime()
+start_time = localtime()
 
-print ("Time start at %s" % time.strftime("%X", start_time) )
+print ("Time start at %s" % strftime("%X", start_time) )
+#print ("Time start at %s" % time.strftime("%X", start_time) )
 
 # wait for user input
 
 raw_input("Please press Enter to continue...")
 
-stop_time = time.localtime()
-difference = time.mktime(stop_time) - time.mktime(start_time)
+stop_time = localtime()
+#stop_time = time.localtime()
 
-print ("Timer stopped at %s" % time.strftime("%X", stop_time))
+#difference = time.mktime(stop_time) - time.mktime(start_time)
+difference = mktime(stop_time) - mktime(start_time)
+
+print ("Timer stopped at %s" % strftime("%X", stop_time))
 print ("Total time %s secondes" % difference)
