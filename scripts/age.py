889ba259675e1fa5609d3fc1b79e86eeadf20a38
kasun <kasunmaduraeng@gmail.com>
2018-01-05 13:36:07 +1300

    adding new scrpit

---
+++ scripts/age.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+### input and raw_input
+
+name = raw_input("What is your name ?")
+birthday = raw_input("what is your birthday ?")
+
+age= input("How old are you")
+
+print ("%s was born on %s" %  (name,birthday))
+print("Half of your age is %s" % (age/2))
+
